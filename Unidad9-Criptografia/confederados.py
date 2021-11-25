e09bf8797d2f01f3a088c2a0e1a02cb65e9caa98
JuanCruzMateos <juanczmt@gmail.com>
2021-11-25 03:24:32 -0300

    crypto

--- Unidad9-Criptografia/confederados.py
+++ Unidad9-Criptografia/confederados.py
@@ -3,15 +3,7 @@ from string import ascii_lowercase
 from pprint import pprint
 
 
-def main():
-    alpha = [char for char in ascii_lowercase]
-    alpha.insert(14, "ñ")
-    valor_letra = dict(zip(alpha, range(1, len(alpha))))
-    letra_valor = dict(zip(range(1, len(alpha)), alpha))
-
-    key = "fe"
-    msg = "hoy es el dia"
-
+def encriptar(alpha: list, valor_letra: dict, letra_valor: dict, key: str, msg: str) -> str:
     i = 0
     encrip = ""
     for char in msg:
@@ -25,7 +17,30 @@ def main():
                 p -= len(alpha)
             i += 1
             encrip += letra_valor[p]
-    print(encrip)
+    return encrip
+
+
+def main():
+    alpha = [char for char in ascii_lowercase]
+    alpha.insert(14, "ñ")
+    valor_letra = dict(zip(alpha, range(1, len(alpha))))
+    letra_valor = dict(zip(range(1, len(alpha)), alpha))
+
+    # key = "fe"
+    # msg = "hoy es el dia"
+    # encrip = NTE JY JQ IÑF
+
+    # msg = "el unico modo de hacer un gran trabajo es amar lo que haces"
+    # ua gvkle ceox fn wqrpa wv vipy ctjqqya nu jbqh wx sdt xpñnu
+    # msg = "es muy simple"
+    # ui xda bxcfwn
+
+    key = "polibio"
+    mensajes = [
+        "el unico modo de hacer un gran trabajo es amar lo que haces", "es muy simple"]
+    for msg in mensajes:
+        encrip = encriptar(alpha, valor_letra, letra_valor, key, msg)
+        print(encrip.upper())
 
 
 if __name__ == "__main__":
