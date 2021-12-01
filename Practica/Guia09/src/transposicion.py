8d1dca8dc2d1edebe78e1fa5d91e5113add696a7
JuanCruzMateos <juanczmt@gmail.com>
2021-12-01 03:14:12 -0300

    transposicion

--- Practica/Guia09/src/transposicion.py
+++ Practica/Guia09/src/transposicion.py
@@ -22,18 +22,48 @@ def main():
         i += 1
     bigList.append(mylist)
 
-    print(bigList)
-
     orden = [5, 2, 1, 3, 4]
+    orden2 = [item-1 for item in orden]
+    # print(bigList)
 
     codificado = ""
     i = 0
     for lista in bigList:
         for i in range(5):
-            codificado += lista[orden[i]-1]
+            codificado += lista[orden2[i]]
 
     print("\n\n")
     print(codificado)
+
+    # decodifico
+    bigList2 = []
+    mylist2 = []
+    i = 0
+    for letra in codificado:
+        mylist2.append(letra)
+        i += 1
+        if i == 5:
+            bigList2.append(mylist2)
+            mylist2 = []
+            i = 0
+    while i != 0 and i < 5:
+        mylist2.append("")
+        i += 1
+    bigList2.append(mylist2)
+
+    decodificado = ""
+    i = 0
+
+    # print("orden2", orden2)
+    for lista in bigList2:
+        zipped_lists = zip(orden2, lista)
+        sorted_zipped_lists = sorted(zipped_lists)
+        sorted_list1 = [element for _, element in sorted_zipped_lists]
+        # print(sorted_zipped_lists)
+        decodificado += "".join(sorted_list1)
+
+    print("\n\n")
+    print(decodificado)
 
 
 if __name__ == "__main__":
