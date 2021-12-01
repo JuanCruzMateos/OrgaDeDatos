55b66335973e7c77a2ef8e0fe8b16f31478cc713
JuanCruzMateos <juanczmt@gmail.com>
2021-12-01 14:02:00 -0300

    transpocicion ok

--- Practica/Guia09/src/transposicion.py
+++ Practica/Guia09/src/transposicion.py
@@ -1,69 +1,71 @@
 #! /usr/bin/python3
-from string import ascii_lowercase
-from string import ascii_uppercase
+
+
+def encode(msg: str, transposicion: list) -> str:
+    # armo tabla
+    i = 0
+    row = []
+    table = []
+    for letra in msg:
+        i += 1
+        row.append(letra)
+        if i == len(transposicion):
+            table.append(row)
+            row = []
+            i = 0
+    while i != 0 and i < len(transposicion):
+        row.append("")
+        i += 1
+    table.append(row)
+
+    # corrijo indices con -1
+    orden = [item-1 for item in transposicion]
+
+    codificado = ""
+    for lista in table:
+        for i in range(len(transposicion)):
+            codificado += lista[orden[i]]
+    return codificado
+
+
+def decode(msg: str, transposicion: list) -> str:
+    i = 0
+    row = []
+    table = []
+    for letra in msg:
+        i += 1
+        row.append(letra)
+        if i == len(transposicion):
+            table.append(row)
+            row = []
+            i = 0
+    while i != 0 and i < len(transposicion):
+        row.append("")
+        i += 1
+    table.append(row)
+
+    orden = [item-1 for item in transposicion]
+
+    decodificado = ""
+    for lista in table:
+        zipped_list = list(zip(orden, lista))
+        sorted_zipped_list = sorted(zipped_list, key=lambda item: item[0])
+        sorted_list1 = [element for _, element in sorted_zipped_list]
+        decodificado += "".join(sorted_list1)
+    return decodificado
 
 
 def main():
-    i = 0
-
     msg = "El cifrado César, es una de las técnicas más simples; consiste en reemplazar cada letra por otra que se encuentra un número fijo de posiciones más adelante."
+    transposicion = [5, 2, 1, 3, 4]
 
-    bigList = []
-    mylist = []
-    for letra in msg:
-        mylist.append(letra)
-        i += 1
-        if i == 5:
-            bigList.append(mylist)
-            mylist = []
-            i = 0
-    while i != 0 and i < 5:
-        mylist.append("")
-        i += 1
-    bigList.append(mylist)
-
-    orden = [5, 2, 1, 3, 4]
-    orden2 = [item-1 for item in orden]
-    # print(bigList)
-
-    codificado = ""
-    i = 0
-    for lista in bigList:
-        for i in range(5):
-            codificado += lista[orden2[i]]
-
-    print("\n\n")
+    codificado = encode(msg, transposicion)
     print(codificado)
 
-    # decodifico
-    bigList2 = []
-    mylist2 = []
-    i = 0
-    for letra in codificado:
-        mylist2.append(letra)
-        i += 1
-        if i == 5:
-            bigList2.append(mylist2)
-            mylist2 = []
-            i = 0
-    while i != 0 and i < 5:
-        mylist2.append("")
-        i += 1
-    bigList2.append(mylist2)
-
-    decodificado = ""
-    i = 0
-
-    # print("orden2", orden2)
-    for lista in bigList2:
-        zipped_lists = zip(orden2, lista)
-        sorted_zipped_lists = sorted(zipped_lists)
-        sorted_list1 = [element for _, element in sorted_zipped_lists]
-        # print(sorted_zipped_lists)
-        decodificado += "".join(sorted_list1)
-
-    print("\n\n")
+    decodificado = decode(codificado, transposicion)
     print(decodificado)
+
+    print(f"msg == decodificado = {msg == decodificado}")
 
 
 if __name__ == "__main__":
